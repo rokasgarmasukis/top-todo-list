eae65f8944e0cf58252c757e6856e1f5006ff4b0
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-03-03 18:00:44 +0100

    Implement basic add task to tasklist

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './css/style.css';
+import Task from './components/Task';
+
+const tasks = [];
 
 // toggle visiblity
 const hideForm = () => {
@@ -6,11 +9,33 @@ const hideForm = () => {
   newTaskForm.classList.toggle('hidden');
 };
 
+const addTask = () => {
+  const title = document.getElementById('title').value;
+  const description = document.getElementById('description').innerText;
+
+  const task = new Task(title, description, 'tomorrow', 'low');
+  tasks.push(task);
+};
+
 const newTask = document.getElementById('new-task');
 newTask.addEventListener('click', hideForm);
+
+const generateTaskMarkup = (task) => {
+  const taskDiv = document.createElement('div');
+  taskDiv.textContent = task.title;
+  return taskDiv;
+};
+
+const showTaskList = () => {
+  const tasksList = tasks.map((task) => generateTaskMarkup(task));
+  const list = document.querySelector('.task-list');
+  list.replaceChildren(...tasksList);
+};
 
 const submitTask = document.getElementById('add-task');
 submitTask.addEventListener('click', (e) => {
   e.preventDefault();
+  addTask();
   hideForm();
+  showTaskList();
 });
